a446946a13133390226e64fcb7c6dbb4d345e998
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2024-07-03 00:01:27 +0200

    rabbit-hole:snapcraft file update

---
+++ rabbit-hole/snap/download_artifacts.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Replace these with your GitHub username, repository name, and asset name
+USERNAME="gentjankolicaj"
+REPO="wonderland"
+ASSET_NAME="artifacts.zip"  # Replace with the actual asset name
+
+# Fetch the latest release information
+latest_release=$(curl -s https://api.github.com/repos/$USERNAME/$REPO/releases/latest)
+
+# Extract the download URL of the asset
+artifact_url=$(echo "$latest_release" | grep -o "\"browser_download_url\":\s*\"[^\"]*\"" | grep "$ASSET_NAME" | cut -d '"' -f 4)
+
+echo "Release artifact url : $artifact_url"
+
+# Download the asset using curl
+curl -L -o $ASSET_NAME $artifact_url
+
+echo "Downloaded release artifact : $ASSET_NAME"
+
+unzip artifacts.zip
+
+echo "Release artifacts unzipped,dir content : $(ls -alh)"
+
+cp ./artifacts/*shaded.jar ./
+
+echo "Deleting ziped file : $(rm ./artifacts.zip)"
+
+echo "Deleting unziped directory: $(rm -rf ./artifacts)"
+
+echo "Dir content : $(ls -alh)"
